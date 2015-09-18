5b364d824e1b9cee72bf5a416d45f5850775fa05
Bert JW Regeer <bertjw@regeer.org>
2015-09-17 21:11:56 -0600

    Project skeleton

---
+++ README.rst
@@ -0,0 +1,38 @@
+==================
+pyramid_authsanity
+==================
+
+An sane defaults auth policy for the `Pyramid Web Framework
+<https://docs.pylonsproject.org/projects/pyramid>`__. that works with `Michael
+Merickel's<http://michael.merickel.org>`__ absolutely fantastic
+`pyramid_services<https://github.com/mmerickel/pyramid_services>`__ to provide
+an easy to use authorization policy that incorporates web security best
+practices.
+
+Installation
+============
+
+Install from `PyPI <https://pypi.python.org/pyramid_authsanity>`__ using
+``pip`` or ``easy_install`` inside a virtual environment.
+
+::
+
+  $ $VENV/bin/pip install pyramid_authsanity
+
+Or install directly from source.
+
+::
+
+  $ git clone https://github.com/usingnamespace/pyramid_authsanity.git
+  $ cd pyramid_authsanity
+  $ $VENV/bin/pip install -e .
+
+Setup
+=====
+
+Activate ``pyramid_authsanity`` by including it into your pyramid application.
+
+::
+
+  config.include('pyramid_authsanity')
+
---
+++ setup.py
@@ -0,0 +1,55 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.rst')).read()
+except IOError:
+    README = CHANGES = ''
+
+requires = [
+    'pyramid',
+    'zope.interface',
+    'pyramid_services'
+]
+
+tests_require = requires + [
+    'coverage',
+    'pytest',
+]
+
+setup(
+    name='pyramid_authsanity',
+    version='0.0.0a0',
+    description='A service layer abstraction for the Pyramid Web Framework.',
+    long_description=README + '\n\n' + CHANGES,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Pyramid',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Programming Language :: Python :: Implementation :: PyPy',
+    ],
+    keywords='pyramid authorization policy',
+    author='Bert JW Regeer',
+    author_email='bertjw@regeer.org',
+    url='https://github.com/usingnamespace/pyramid_authsanity',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=requires,
+    extras_require={
+        'testing': tests_require,
+    },
+    tests_require=tests_require,
+    test_suite='pyramid_authsanity',
+    entry_points={
+    },
+)
