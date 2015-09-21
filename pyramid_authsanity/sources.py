33f76d4399312ae2ef0608077ed393b951db1591
Bert JW Regeer <bertjw@regeer.org>
2015-09-20 21:46:25 -0600

    Use a decorator to de-indent the code

--- pyramid_authsanity/sources.py
+++ pyramid_authsanity/sources.py
@@ -9,36 +9,34 @@ from .interfaces import (
         IAuthSourceService,
         )
 
+from .util import InnerFactory
+
 def SessionAuthSourceInitializer(value_key='sanity.'):
     """ An authentication source that uses the current session """
 
     value_key = value_key + 'value'
 
-    def SessionAuthSourceFactory(context, request):
-        @implementer(IAuthSourceService)
-        class SessionAuthSource(object):
-            """ An authentication source that uses the current session """
+    @implementer(IAuthSourceService)
+    class SessionAuthSource(object):
+        vary = []
 
-            vary = []
+        def __init__(self, context, request):
+            self.request = request
+            self.session = request.session
 
-            def __init__(self, context, request):
-                self.request = request
-                self.session = request.session
+        def get_value(self):
+            return self.session.get(value_key, [None, None])
 
-            def get_value(self):
-                return self.session.get(value_key, [None, None])
+        def headers_remember(self, value):
+            self.session[value_key] = value
+            return []
 
-            def headers_remember(self, value):
-                self.session[value_key] = value
-                return []
+        def headers_forget(self):
+            if value_key in self.session:
+                del self.session[value_key]
+            return []
 
-            def headers_forget(self):
-                if value_key in self.session:
-                    del self.session[value_key]
-                return []
-
-        return SessionAuthSource(context, request)
-    return SessionAuthSourceFactory
+    return InnerFactory(SessionAuthSource)
 
 
 def CookieAuthSourceInitializer(
@@ -56,47 +54,44 @@ def CookieAuthSourceInitializer(
         ):
     """ An authentication source that uses a unique cookie """
 
-    def CookieAuthSourceFactory(context, request):
-        @implementer(IAuthSourceService)
-        class CookieAuthSource(object):
+    @implementer(IAuthSourceService)
+    class CookieAuthSource(object):
+        vary = ['Cookie']
 
-            vary = ['Cookie']
+        def __init__(self, context, request):
+            self.domains = domains
 
-            def __init__(self, context, request):
-                self.domains = domains
+            if self.domains is None:
+                self.domains = []
+                self.domains.append(request.domain)
 
-                if self.domains is None:
-                    self.domains = []
-                    self.domains.append(request.domain)
+            self.cookie = SignedCookieProfile(
+                            secret,
+                            'authsanity',
+                            cookie_name,
+                            secure=secure,
+                            max_age=max_age,
+                            httponly=httponly,
+                            path=path,
+                            domains=domains,
+                            hashalg=hashalg,
+                            )
+            # Bind the cookie to the current request
+            self.cookie = self.cookie.bind(request)
 
-                self.cookie = SignedCookieProfile(
-                                secret,
-                                'authsanity',
-                                cookie_name,
-                                secure=secure,
-                                max_age=max_age,
-                                httponly=httponly,
-                                path=path,
-                                domains=domains,
-                                hashalg=hashalg,
-                                )
-                # Bind the cookie to the current request
-                self.cookie = self.cookie.bind(request)
+        def get_value(self):
+            val = self.cookie.get_value()
 
-            def get_value(self):
-                val = self.cookie.get_value()
+            if val is None:
+                return [None, None]
 
-                if val is None:
-                    return [None, None]
+            return val
 
-                return val
+        def headers_remember(self, value):
+            return self.cookie.get_headers(value, domains=self.domains)
 
-            def headers_remember(self, value):
-                return self.cookie.get_headers(value, domains=self.domains)
+        def headers_forget(self):
+            return self.cookie.get_headers('', max_age=0)
 
-            def headers_forget(self):
-                return self.cookie.get_headers('', max_age=0)
-
-        return CookieAuthSource(context, request)
-    return CookieAuthSourceFactory
+    return InnerFactory(CookieAuthSource)
 
