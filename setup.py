a45c7cdfde2c424f746b306f7e397bebbb5e28a1
Bert JW Regeer <bertjw@regeer.org>
2015-10-12 22:25:44 -0600

    Drop Python 2.6 support
    I was going to support it until errors showed up due to using Python
    2.7<= syntax. That time has come.

--- setup.py
+++ setup.py
@@ -39,7 +39,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.2',
         'Programming Language :: Python :: 3.3',
