1b4da49ef5e1257e3e36270f2eeda1d73aeee385
Bert JW Regeer <bertjw@regeer.org>
2017-10-29 16:18:02 -0600

    Add Authorization header based source

--- src/pyramid_authsanity/sources.py
+++ src/pyramid_authsanity/sources.py
@@ -1,14 +1,18 @@
 from webob.cookies import (
+    JSONSerializer,
     SignedCookieProfile,
-    )
+    SignedSerializer,
+)
 
 from zope.interface import implementer
 
 from .interfaces import (
     IAuthSourceService,
-    )
+)
 
-def SessionAuthSourceInitializer(value_key='sanity.'):
+def SessionAuthSourceInitializer(
+    value_key='sanity.'
+):
     """ An authentication source that uses the current session """
 
     value_key = value_key + 'value'
@@ -56,7 +60,7 @@ def CookieAuthSourceInitializer(
     domains=None,
     debug=False,
     hashalg='sha512',
-    ):
+):
     """ An authentication source that uses a unique cookie. """
 
     @implementer(IAuthSourceService)
@@ -99,3 +103,59 @@ def CookieAuthSourceInitializer(
             return self.cookie.get_headers(None, max_age=0)
 
     return CookieAuthSource
+
+
+def HeaderAuthSourceInitializer(
+    secret,
+    salt='sanity.header.'
+):
+    @implementer(IAuthSourceService)
+    class HeaderAuthSource(object):
+        vary = ['Authorization']
+
+        def __init__(self, context, request):
+            self.request = request
+            self.cur_val = None
+
+            serializer = JSONSerializer()
+            self.serializer = SignedSerializer(
+                secret,
+                salt,
+                serializer=serializer,
+                )
+
+        def _get_authorization(self):
+            try:
+                type, token = self.request.authorization
+
+                return self.serializer.loads(token)
+            except Exception:
+                return None
+
+        def _create_authorization(self, value):
+            try:
+                return self.serializer.dumps(value)
+            except Exception:
+                return ''
+
+        def get_value(self):
+            if self.cur_val is None:
+                self.cur_val = self._get_authorization() or [None, None]
+
+            return self.cur_val
+
+        def headers_remember(self, value):
+            if self.cur_val is None:
+                self.cur_val = None
+
+            token = self._create_authorization(value)
+            auth_info = str(b'Bearer ' + token, 'latin-1', 'strict')
+            return [('Authorization', auth_info)]
+
+        def headers_forget(self):
+            if self.cur_val is None:
+                self.cur_val = None
+
+            return []
+
+    return HeaderAuthSource
